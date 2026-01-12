5ae3efff85168d4fa92f3f1fb26361b4101f2455
Imtiaz <mdimtiazzihad@gmail.com>
2026-01-12 23:04:43 +0600

    feat: add schedule and doctor_schedule models with relationships, and update user model to include doctorSchedules

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -35,8 +35,8 @@ const createAdmin = catchAsync(async (req: Request, res: Response) => {
   });
 });
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
-  const filters = pick(req.query, userFilterableFields);
-  const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+  const filters = pick(req.query, userFilterableFields); // Searching, filtering 
+  const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]); // pagination & sorting 
 
   const result = await UserService.getAllFromDB(filters,options);
   sendResponse(res, {
