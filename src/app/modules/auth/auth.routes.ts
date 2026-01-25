caabb6f995d93539ffc2de25bacb077a2e281e70
Imtiaz <mdimtiazzihad@gmail.com>
2026-01-25 20:16:43 +0600

    feat(auth): add email functionality for password reset and enhance authentication features
    - Added nodemailer for sending emails and created emailSender utility.
    - Implemented forgotPassword and resetPassword methods in AuthService.
    - Enhanced AuthController with new endpoints for password management and user retrieval.
    - Updated auth.routes to include new routes for password change, forgot password, and reset password.
    - Improved login and refreshToken methods to handle token generation and validation.
    - Updated package.json to include nodemailer and its types.
    - Modified config to include email sender credentials.

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,7 +1,23 @@
 import express, { NextFunction, Request, Response } from "express";
 import { AuthController } from "./auth.controller";
+import { UserRole } from "../../generated/prisma/enums";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
+router.get(
+    "/me",
+    AuthController.getMe
+)
+router.post("/login", AuthController.login);
+router.post("/refresh-token", AuthController.refreshToken);
 
-router.post("/login",AuthController.login);
+router.post(
+  "/change-password",
+  auth(UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+  AuthController.changePassword,
+);
+
+router.post("/forgot-password", AuthController.forgotPassword);
+
+router.post("/reset-password", AuthController.resetPassword);
 export const authRoutes = router;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,24 +1,37 @@
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({ path: path.join(process.cwd(), '.env') });
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
-    node_env: process.env.NODE_ENV,
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-    cloudinary:{
-        api_secret: process.env.CLOUDINARY_API_SECRET,
-        cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-        api_key:process.env.CLOUDINARY_API_KEY
-    },
-    token:{
-        secret: process.env.JWT_SECRET as string,
-        expireIn: process.env.JWT_EXPIRES_IN as string,
-        algorithm: process.env.JWT_ALGORITHM as string
-    },
-    openRouterApiKey: process.env.OPENROUTER_API_KEY,
-    stripeSecretkey: process.env.STRIPE_SECRET_KEY,
-    stripeWebHookSecret: process.env.STRIPE_WEB_HOOK_SECRET,
-
-}
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  cloudinary: {
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+  },
+  token: {
+    secret: process.env.JWT_SECRET as string,
+    expireIn: process.env.JWT_EXPIRES_IN as string,
+    algorithm: process.env.JWT_ALGORITHM as string,
+  },
+  openRouterApiKey: process.env.OPENROUTER_API_KEY,
+  stripeSecretkey: process.env.STRIPE_SECRET_KEY,
+  stripeWebHookSecret: process.env.STRIPE_WEB_HOOK_SECRET,
+  emailSender: {
+    email: process.env.EMAIL,
+    app_pass: process.env.APP_PASS,
+  },
+  jwt: {
+    jwt_secret: process.env.JWT_SECRET,
+    expires_in: process.env.EXPIRES_IN,
+    refresh_token_secret: process.env.REFRESH_TOKEN_SECRET,
+    refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    reset_pass_secret: process.env.RESET_PASS_TOKEN,
+    reset_pass_token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN,
+  },
+  salt_round: process.env.SALT_ROUND,
+  reset_pass_link: process.env.RESET_PASS_LINK,
+};
